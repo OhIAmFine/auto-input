f2d57c35b34f060bd09eadf939ee10810c59690d
OhIAmFine <845782625@qq.com>
2020-05-19 11:39:25 +0800

    feat: resole th first version

--- background.js
+++ background.js
@@ -1,41 +1,5 @@
 chrome.commands.onCommand.addListener(function (command) {
-  // alert(command)
-  // console.log(command)
-  console.log("------------");
-  Array.prototype.slice
-    .call(document.getElementsByTagName("input"))
-    .forEach((e) => (e.value = "1"));
-
-  const code = `(function getUrls(){
-        const forkUrl = document.querySelector('meta[name="octolytics-dimension-repository_parent_nwo"]') 
-          ? document.querySelector('meta[name="octolytics-dimension-repository_parent_nwo"]').content
-          : undefined;
-        const url = document.querySelector('meta[name="go-import"]') 
-          ? document.querySelector('meta[name="go-import"]').content.split(' ')[0]
-          : undefined;
-        const href = window.location.href;
-        return { forkUrl, url, href };
-      })()`;
-  chrome.tabs.executeScript(undefined, { code }, function (result) {
-    // alert(document);
-    console.log('--------', document)
-
-//     const { forkUrl, url, href } = result[0];
-//     if (forkUrl) {
-//       document.getElementById("url").innerText = `
-//             git remote add upstream https://github.com/${forkUrl}
-//             git fetch upstream
-//             git branch --set-upstream-to=upstream/master master`;
-//     } else if (url) {
-//       document.getElementById("url").innerText = `
-//             git remote add upstream https://${url}
-//             git fetch upstream
-//             git branch --set-upstream-to=upstream/master master`;
-//     } else {
-//       document.getElementById("url").innerText = `
-//             git remote add upstream ${href}
-//             git fetch upstream
-//             git branch --set-upstream-to=upstream/master master`;
-//     }
+  chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+    chrome.tabs.sendMessage(tabs[0].id, { type: "set-random-value" });
   });
 });
--- content.js
+++ content.js
@@ -1,3 +1,36 @@
-chrome.commands.onCommand.addListener(function (command) {
-  console.log(document.getElementsByTagName("input").length);
+chrome.runtime.onMessage.addListener(function (message) {
+  if (message.type === "set-random-value") {
+    Array.prototype.slice
+      .call(document.getElementsByTagName("input"))
+      .forEach((e) => {
+        if (e.disabled || e.type === "password") return;
+        const maxLen = 10;
+        const randomLen = Math.floor(Math.random() * maxLen + 1);
+
+        if (e.type === "number") {
+          e.value = new Array(randomLen)
+            .fill(0)
+            .map(() =>
+              String.fromCharCode(48 + Math.floor(Math.random() * 8 + 1))
+            )
+            .join("");
+          return;
+        }
+        const randomStr = new Array(randomLen)
+          .fill(0)
+          .map(() => {
+            let randomNum = 48 + Math.floor(Math.random() * 73 + 1);
+            while (
+              (randomNum > 90 && randomNum < 97) ||
+              (randomNum > 57 && randomNum < 65)
+            ) {
+              randomNum = 48 + Math.floor(Math.random() * 73 + 1);
+            }
+            return String.fromCharCode(randomNum);
+          })
+          .join("");
+
+        e.value = randomStr;
+      });
+  }
 });
